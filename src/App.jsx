236894edd3953fa9120c547323c1a291694868bc
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2023-09-28 12:16:09 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,33 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Card from "./components/Card";
-import Button from "./elements/button";
-import TextInput from "./elements/TextInput";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-  const [submittedName, setSubmittedName] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Nama Anda: ", name);
-    setSubmittedName(name); // Update the submitted name
-  };
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
 
   return (
-    <Router>
-      {/* <div>
+    <>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div> */}
-      
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+
 
       <div className="main">
 
-      <h1>Praktikum RPLBK - Modul 2</h1>
-      <h1>Kelompok 11</h1>
+      <h1>Praktikum RPLBK - Modul 4</h1>
+      <h1>Kelompok 08</h1>
 
-        <Card />
-        <form action="" onSubmit={handleSubmit}>
-          <TextInput
-            border="aquamarine"
-            placeholder="Nama"
-            value={name}
-            onChange={handleNameChange}
-          />
-          <Button background="aquamarine" type="submit">
-            Kirim
-          </Button>
-        </form>
-        {submittedName && (
-          <div>
-            <p>Hallo,{submittedName}</p>
-          </div>
-        )}
+        <Card></Card>
+       
+      
       </div>
-    </Router>
+    </>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../App.css";
 import Button from "../elements/button";
+import TextInput from "../elements/TextInput";
 
 function Square(props) {
   return (
@@ -19,44 +20,77 @@ function Square(props) {
     </div>
   );
 }
-export default class Card extends Component {
-  render() {
-    const bgColor = {
-      backgroundColor: this.props.bgColor,
-    };
-    return (
-      <div className="User-cards">
-        <div className="User-info" style={bgColor}>
+
+export default function Card() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const cardsData = [
+    {
+      nim: "21120120120019",
+      nama: "Adivia Khusnul Aisha",
+      profile: "https://avatars.githubusercontent.com/u/82595554?v=4",
+      email: "adiviakaa@gmail.com",
+      github: "https://github.com/mdiph",
+    },
+    {
+      nim: "21120120140158",
+      nama: "Faizal Lutfi Yoga Triadi",
+      profile: "https://avatars.githubusercontent.com/u/42670670?v=4",
+      email: "faizal2jz@gmail.com",
+      github: "https://github.com/mdiph",
+    },
+    {
+      nim: "21120120140054",
+      nama: "Muhammad Dhiva Pradigta",
+      profile: "https://avatars.githubusercontent.com/u/82644062?v=4",
+      email: "dhivapradigta96@gmail.com",
+      github: "https://github.com/mdiph",
+    },
+    {
+      nim: "21120120120034",
+      nama: "Muhammad Hafizh Zikry",
+      profile: "https://avatars.githubusercontent.com/u/81897441?v=4",
+      email: "hafizhcool24@gmail.com",
+      github: "https://github.com/mdiph",
+    },
+  ];
+
+  
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filter daftar komponen Square berdasarkan kata kunci pencarian
+  const filteredCards = cardsData.filter((card) =>
+    card.nama.toLowerCase().includes(searchTerm.toLowerCase()) || card.nim.includes(searchTerm) ||
+    card.email.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="User-cards">
+      
+      <div className="User-info">
+        {filteredCards.map((card, index) => (
           <Square
-            nim="21120120120019"
-            nama="Adivia Khusnul Aisha"
-            profile="https://avatars.githubusercontent.com/u/82595554?v=4"
-            email="adiviakaa@gmail.com"
-            github="https://github.com/mdiph"
+            key={index}
+            nim={card.nim}
+            nama={card.nama}
+            profile={card.profile}
+            email={card.email}
+            github={card.github}
           />
-          <Square
-            nim="21120120140158"
-            nama="Faizal Lutfi Yoga Triadi"
-            profile="https://avatars.githubusercontent.com/u/42670670?v=4"
-            email="faizal2jz@gmail.com"
-            github="https://github.com/mdiph"
-          />
-          <Square
-            nim="21120120140054"
-            nama="Muhammad Dhiva Pradigta"
-            profile="https://avatars.githubusercontent.com/u/82644062?v=4"
-            email="dhivapradigta96@gmail.com"
-            github="https://github.com/mdiph"
-          />
-          <Square
-            nim="21120120120034"
-            nama="Muhammad Hafizh Zikry"
-            profile="https://avatars.githubusercontent.com/u/81897441?v=4"
-            email="hafizhcool24@gmail.com"
-            github="https://github.com/mdiph"
-          />
-        </div>
+        ))}
       </div>
-    );
-  }
+
+      <form action="" >
+          <TextInput
+            border="aquamarine"
+            placeholder="Nama"
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </form>
+    </div>
+  );
 }
